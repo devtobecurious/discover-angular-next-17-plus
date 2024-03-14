8081c99f1f79e3b8b673696ba90fc4e074403278
Evan BOISSONNOT <evan@boissonnot.fr>
2024-03-14 17:46:20 +0100

    feat: prepare to add movie

--- discover-angular-17-next/projects/videotheque-sw/src/app/app.routes.ts
+++ discover-angular-17-next/projects/videotheque-sw/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'movies',
+    loadChildren: () => import('./features/movies/movies.routes').then(item => item.movieRoutes)
+  }
+];
